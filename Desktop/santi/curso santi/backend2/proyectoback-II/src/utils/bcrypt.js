44827e45d61d13e4cfff0d13f290e94f20d304c3
Santiago Zorrilla <santiagozorrilla5@gmail.com>
2025-06-03 11:49:06 -0300

    Delete Desktop directory

--- Desktop/santi/curso santi/backend2/proyectoback-II/src/utils/bcrypt.js
+++
@@ -1,9 +0,0 @@
-import { hashSync, compareSync } from "bcrypt";
-
-export const hashPassword = (password) => hashSync(password, 6)
-
-export const validatePassword = (password, passwordBDD) => compareSync(password, passwordBDD)
-
-const passE = hashPassword("Pecas622")
-console.log(passE);
-console.log(validatePassword("Pecas622", passE));
